afb2ab5ecf3efd361ebf4f50efa206e04a333c56
dasom <dasom589@gmail.com>
2024-01-15 16:12:10 +0900

    2024-01-15 6교시

--- src/com/javaex/ex01/Ex02.java
+++ src/com/javaex/ex01/Ex02.java
@@ -1,30 +1,31 @@
 package com.javaex.ex01;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-public class Ex01 {
+public class Ex02 {
 
 	public static void main(String[] args) throws IOException {
 		
 		InputStream in=new FileInputStream("C:\\javaStudy\\img.jpg");
-		OutputStream out=new FileOutputStream("C:\\javaStudy\\byteimg.jpg");
+		OutputStream out=new FileOutputStream("C:\\javaStudy\\byteBuffImg.jpg");
+		
+		byte[] buff=new byte[1024];
 		
 		System.out.println("복사 시작");
-		
 		while (true) {
-			int data=in.read();
-			System.out.println(data);
+			int data=in.read(buff);
 			
 			if (data==-1) {
-				System.out.println("복사 끝"+data);
+				System.out.println("복사 끝");
 				break;
 			}
 			
-			out.write(data);
+			out.write(buff);
 		}
 		
 		in.close();
