714122852958969135877e55a673a871342ff78e
dasom <dasom589@gmail.com>
2024-01-15 15:11:42 +0900

    2024-01-15 5교시

---
+++ src/com/javaex/ex01/Ex01.java
@@ -0,0 +1,14 @@
+package com.javaex.ex01;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		
+		InputStream in=new FileInputStream();
+
+	}
+
+}
